d5dbbb6cac6e0741bf7aae95a4268c6b1ca6a7c8
mahditimore <mahditimorese@gmail.com>
2025-12-26 17:30:06 -0800

    Update tamrin_s_7.py

--- tamrin_s_7.py
+++ tamrin_s_7.py
@@ -1,35 +1,47 @@
 from abc import ABC, abstractmethod
 import math
-#/////////////////////////////////////////////////////
+
+# /////////////////////////////////////////////////////
 class Shape(ABC):
     @abstractmethod
     def calculate_area(self):
-        pass  
+        pass
+
     @abstractmethod
     def calculate_perimeter(self):
         pass
-#/////////////////////////////////////////////////////////
+
+
+# ///////////////////////////////////////////////////////////
 class Rectangle(Shape):
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
+    def __init__(self, rectangle_width, rectangle_height):
+        self.rectangle_width = rectangle_width
+        self.rectangle_height = rectangle_height
 
     def calculate_area(self):
-        return self.width * self.height
+        return self.rectangle_width * self.rectangle_height
+
     def calculate_perimeter(self):
-        return 2 * (self.width + self.height)
-#///////////////////////////////////////////////////////
+        return 2 * (self.rectangle_width + self.rectangle_height)
+
+
+# /////////////////////////////////////////////////////////
 class Circle(Shape):
-    def __init__(self, radius):
-        self.radius = radius
+    def __init__(self, circle_radius):
+        self.circle_radius = circle_radius
+
     def calculate_area(self):
-        return 3.14159 * self.radius * self.radius
+        return math.pi * self.circle_radius ** 2
+
     def calculate_perimeter(self):
-        return 2 * 3.14159 * self.radius
-#////////////////////////////////////////////////
-shapes = [ Rectangle(4, 5), Circle(3) ]
-#/////////////////////////////////////////////////
-for shape in shapes:
+        return 2 * math.pi * self.circle_radius
+
+
+# //////////////////////////////////////////////////
+shapes_list = [Rectangle(4, 5), Circle(3)]
+
+# ///////////////////////////////////////////////////
+for shape in shapes_list:
     print("Area:", shape.calculate_area())
     print("Perimeter:", shape.calculate_perimeter())
     print("-------------")
