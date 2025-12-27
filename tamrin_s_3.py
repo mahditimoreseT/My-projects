62838baf1ec173f9f9eeadd0bbbb1a6be904ac77
mahditimore <mahditimorese@gmail.com>
2025-12-26 17:19:49 -0800

    Update tamrin_s_3.py

--- tamrin_s_3.py
+++ tamrin_s_3.py
@@ -1,17 +1,17 @@
-# tarif list daneshjuyan
-students_list = [
-    {"student_id": 1, "name": "Ali", "grades": [18, 19, 20]},
-    {"student_id": 2, "name": "Zahra", "grades": [15, 14, 16.5]},
-    {"student_id": 3, "name": "Mehdi", "grades": [17, 18, 19]},
-]
 
-# tabe mohasbeh madel  daneshju
+# تابع محاسبه معدل دانشجو
 def calculate_student_gpa(student):
-    grades = student["grades"]
+    grades = student["grades_list"]
     gpa = sum(grades) / len(grades)
     return gpa
 
-# halgheh baraye namayesh madel  daneshju
+# تعریف لیست دانشجویان
+students_list = [
+    {"student_id": 1, "student_name": "Ali", "grades_list": [18, 19, 20]},
+    {"student_id": 2, "student_name": "Zahra", "grades_list": [15, 14, 16.5]},
+    {"student_id": 3, "student_name": "Mehdi", "grades_list": [17, 18, 19]},
+]
+# حلقه برای نمایش معدل دانشجویان
 for student in students_list:
-    avg = calculate_student_gpa(student)
-    print(f"Name: {student['name']} | GPA: {avg:.2f}")
+    average_gpa = calculate_student_gpa(student)
+    print(f"Name: {student['student_name']} | GPA: {average_gpa:.2f}")
