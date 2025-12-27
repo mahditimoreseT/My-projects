51ee81575f7b385ae583ce70cd1ed67f69403645
mahditimore <mahditimorese@gmail.com>
2025-12-26 17:29:16 -0800

    Update tamrin_s_6.py

--- tamrin_s_6.py
+++ tamrin_s_6.py
@@ -1,36 +1,42 @@
-#/////////////////////////////////////////////////////////
+# /////////////////////////////////////////////////////////
 class Vehicle:
-    def __init__(self, brand, year):
-        self.brand = brand
-        self.year = year
+    def __init__(self, vehicle_brand, manufacture_year):
+        self.vehicle_brand = vehicle_brand
+        self.manufacture_year = manufacture_year
 
     def display_info(self):
-        print(f"Brand: {self.brand}")
-        print(f"Year: {self.year}")
-#//////////////////////////////////////////////////////
+        print(f"Brand: {self.vehicle_brand}")
+        print(f"Year: {self.manufacture_year}")
+
+
+# ////////////////////////////////////////////////////////
 class Car(Vehicle):
-    def __init__(self, brand, year, num_doors):
-        super().__init__(brand, year)  
-        self.num_doors = num_doors    
+    def __init__(self, vehicle_brand, manufacture_year, number_of_doors):
+        super().__init__(vehicle_brand, manufacture_year)
+        self.number_of_doors = number_of_doors
 
     def display_info(self):
-        super().display_info() 
-        print(f"Number of doors: {self.num_doors}")
-#///////////////////////////////////////////////////////
+        super().display_info()
+        print(f"Number of doors: {self.number_of_doors}")
+
+
+# /////////////////////////////////////////////////////////
 class Motorcycle(Vehicle):
-    def __init__(self, brand, year, has_sidecar):
-        super().__init__(brand, year)
-        self.has_sidecar = has_sidecar  
+    def __init__(self, vehicle_brand, manufacture_year, has_sidecar):
+        super().__init__(vehicle_brand, manufacture_year)
+        self.has_sidecar = has_sidecar
 
     def display_info(self):
         super().display_info()
         print(f"Has sidecar: {self.has_sidecar}")
-#//////////////////////////////////////////////////////////////
+
+
+# ////////////////////////////////////////////////////////////////
 car1 = Car("BMW", 2020, 4)
-moto1 = Motorcycle("Honda", 2018, False)
+motorcycle1 = Motorcycle("Honda", 2018, False)
 
 print("Car Info:")
 car1.display_info()
 
 print("\nMotorcycle Info:")
-moto1.display_info()
+motorcycle1.display_info()
